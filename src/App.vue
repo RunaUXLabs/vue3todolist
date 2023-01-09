07773e730f50d7ee2b54504d04f04d4cd1ef0585
Runa <runa.uxlabs@gmail.com>
2023-01-09 18:39:02 +0900

    vue기본셋팅

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="app">
+		<TodoHeader></TodoHeader>
+		<TodoInput></TodoInput>
+		<TodoList></TodoList>
+		<TodoFooter></TodoFooter>
+	</div>
+</template>
+
+<script>
+
+export default {
+    components: { TodoHeader, TodoInput, TodoList, TodoFooter }
+};
+import TodoHeader from '../components/TodoHeader.vue';
+import TodoInput from '../components/TodoInput.vue';
+import TodoList from '../components/TodoList.vue';
+import TodoFooter from '../components/TodoFooter.vue';
+</script>
+
+<style>
+</style>
